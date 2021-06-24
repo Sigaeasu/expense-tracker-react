8bbb1b78ca1acd284c9a90abac81d722284e80b2
sigaeasu <sigikemahduta@gmail.com>
2021-06-24 22:11:38 +0700

    section 4 clear

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import './Expenses.css';
+
+const Expenses = (props) => {
+    const expenses = props.item;
+
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    
+    return (
+        <Card className='expenses'>
+            <ExpensesFilter 
+                selectedYear={filteredYear} 
+                onChangeFilter={filterChangeHandler}
+            />   
+            <ExpenseItem 
+                title={expenses[0].title}
+                amount={expenses[0].amount}
+                date={expenses[0].date}
+            /> 
+            <ExpenseItem 
+                title={expenses[1].title}
+                amount={expenses[1].amount}
+                date={expenses[1].date}
+            /> 
+            <ExpenseItem 
+                title={expenses[2].title}
+                amount={expenses[2].amount}
+                date={expenses[2].date}
+            /> 
+            <ExpenseItem 
+                title={expenses[3].title}
+                amount={expenses[3].amount}
+                date={expenses[3].date}
+            /> 
+        </Card>   
+    )
+}
+
+export default Expenses;
